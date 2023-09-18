2f2e21aab13ca0ecc7ac5554b334b02453de56ba
Sene <56192670+Sene68@users.noreply.github.com>
2023-09-18 20:44:38 +0900

    상태에 따른 특정 아이템 조회

--- src/main/java/com/example/revise/repository/ItemRepository.java
+++ src/main/java/com/example/revise/repository/ItemRepository.java
@@ -1,10 +1,14 @@
 package com.example.revise.repository;
 
 import com.example.revise.entity.Item;
+import com.example.revise.enums.ItemStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
     Optional<Item> findByItemCode(String itemCode);
+
+    Optional<List<Item>> findAllByItemCodeAndItemStatus(String ItemCode, ItemStatus itemStatus);
 }
